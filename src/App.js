e130459fac5243c4c182dbcecda8713c9f815b12
Marco Ye <marco.ye99@gmail.com>
2022-07-01 13:02:38 -0400

    initial video click handler code

--- src/App.js
+++ src/App.js
@@ -6,27 +6,32 @@ import MainVideoData from '../src/components/MainVideoData/MainVideoData';
 import Form from '../src/components/Form/Form';
 import Comments from '../src/components/Comments/Comments';
 import NextVideos from '../src/components/NextVideos/NextVideos';
+import NextVideosList from './data/videos.json'
+import videoDetails from './data/video-details.json'
+
 
 
 
 class App extends React.Component {
-  render () {
-
-  return (
-    <body>
-      <header className= 'header'>
-        <Header/>
-      </header>
-      <MainVideo />
-      <MainVideoData />
-      <Form />
-      <Comments />
-      <NextVideos />
-    </body>
-  )
-
+  state = {
+    selectedVideo: videoDetails[0],
+    videoList: NextVideosList,
   }
 
-}
+  render () {
 
-export default App;
+
+    return (
+      <body>
+        <header className= 'header'>
+          <Header/>
+        </header>
+        <MainVideo />
+        <MainVideoData />
+        <Form />
+        <Comments />
+        <NextVideos />
+      </body>
+    )
+  }
+}
