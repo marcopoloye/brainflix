0ec4ce6cb31a437e15817b5bbe979d5a19ae709e
Marco Ye <marco.ye99@gmail.com>
2022-07-02 19:47:14 -0400

    finished form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,16 +5,17 @@ import './Form.scss';
 const Form = (props) => {
     return (
         <>
-            <div className= 'form__container'>
-                <p>{props.selectedVideo.comments.length} Comments</p>
-                <div>
-                    <img className='form__avatar' src={avatar}/>
+            <div className='form'>
+                <p className='form__comment-count'>{props.selectedVideo.comments.length} Comments</p>
+                <div className='form__container'>
+                    <div className='form__avatar'>
+                    </div>
+                    <form className='form__section'>
+                        <p className='form__title'>Join the conversation</p>
+                        <input className='form__input' type='textarea' placeholder='Add a new comment'/>
+                        <button className='form__button' type='submit'>Comment</button>
+                    </form>
                 </div>
-                <form>
-                    <p>Join the conversation</p>
-                    <input type='text' placeholder='Add a new comment'/>
-                    <button type='submit'>Comment</button>
-                </form>
             </div>
         </>
     )
