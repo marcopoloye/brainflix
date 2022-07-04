15c8a1501b01ef0f02a4bd7ce744207c54596861
Marco Ye <marco.ye99@gmail.com>
2022-07-03 22:17:14 -0400

    added player controls

--- src/components/MainVideo/MainVideo.js
+++ src/components/MainVideo/MainVideo.js
@@ -4,8 +4,7 @@ import './MainVideo.scss';
 const MainVideo = (props) => {
     return (
         <div className='main-video__container'>
-            <video poster= {props.selectedVideo.image} className= 'main-video'>
-                <button className='main-video__controls-play'>text</button>
+            <video controls poster= {props.selectedVideo.image} className='main-video'>
             </video>
         </div>
     )
