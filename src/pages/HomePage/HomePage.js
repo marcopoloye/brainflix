3f6cd9bbd5713c439f0a7b7a9f115b8e88405e72
Marco Ye <marco.ye99@gmail.com>
2022-07-17 00:35:31 -0400

    removed indent

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -10,90 +10,90 @@ import './HomePage.scss'
 
 
 class HomePage extends React.Component {
-    state = {
-        selectedVideo: '',
-        videoList: [],
-      }
+  state = {
+      selectedVideo: '',
+      videoList: [],
+    }
 
-    componentDidMount() {
+  componentDidMount() {
 
-      const currentVideoId = this.props.match.params.videoId;
+    const currentVideoId = this.props.match.params.videoId;
 
-      axios.get(`http://localhost:8080/videos`)
-        .then ((response) => {
+    axios.get(`http://localhost:8080/videos`)
+      .then ((response) => {
 
-          const videoListResults = response.data;
-          const firstVideoId = videoListResults[0].id;
+        const videoListResults = response.data;
+        const firstVideoId = videoListResults[0].id;
 
-          this.setState({
-            videoList: videoListResults
-          });
+        this.setState({
+          videoList: videoListResults
+        });
 
-          return (currentVideoId) ? currentVideoId : firstVideoId;
+        return (currentVideoId) ? currentVideoId : firstVideoId;
+      })
+      .then ((firstVideoId) => {
+        return axios.get(`http://localhost:8080/videos/${firstVideoId}`)
+      })
+      .then ((firstVideoData) => {
+        this.setState({
+          selectedVideo: firstVideoData.data
         })
-        .then ((firstVideoId) => {
-          return axios.get(`http://localhost:8080/videos/${firstVideoId}`)
-        })
-        .then ((firstVideoData) => {
-          this.setState({
-            selectedVideo: firstVideoData.data
+      })
+      .catch((error) => {'Error retrieving data'})
+  }
+
+  componentDidUpdate(prevProps) {
+
+    const currentVideoId = this.props.match.params.videoId;
+    const prevVideoId = prevProps.match.params.videoId;
+
+    const videoIdToFetch = (currentVideoId) ? currentVideoId : this.state.videoList[0].id
+
+    if (prevVideoId !== currentVideoId) {
+      axios.get(`http://localhost:8080/videos/${videoIdToFetch}`)
+        .then ((videoData) => {
+          this.setState ({
+            selectedVideo: videoData.data
           })
         })
         .catch((error) => {'Error retrieving data'})
     }
+  }
+    
+  render() {
+    const filteredVideos = this.state.videoList.filter(video => video.id !== this.state.selectedVideo.id)
 
-    componentDidUpdate(prevProps) {
+    const timestamp = this.state.selectedVideo.timestamp;
+    const betterDate = new Date(timestamp);
+    const bestDate = (betterDate.getMonth()+1) + '/' + betterDate.getDate() + '/' + betterDate.getFullYear();
 
-      const currentVideoId = this.props.match.params.videoId;
-      const prevVideoId = prevProps.match.params.videoId;
-
-      const videoIdToFetch = (currentVideoId) ? currentVideoId : this.state.videoList[0].id
-
-      if (prevVideoId !== currentVideoId) {
-        axios.get(`http://localhost:8080/videos/${videoIdToFetch}`)
-          .then ((videoData) => {
-            this.setState ({
-              selectedVideo: videoData.data
-            })
-          })
-          .catch((error) => {'Error retrieving data'})
-      }
-    }
-      
-    render() {
-      const filteredVideos = this.state.videoList.filter(video => video.id !== this.state.selectedVideo.id)
-
-      const timestamp = this.state.selectedVideo.timestamp;
-      const betterDate = new Date(timestamp);
-      const bestDate = (betterDate.getMonth()+1) + '/' + betterDate.getDate() + '/' + betterDate.getFullYear();
-
-      return (
-        <>
-          <section>
+    return (
+      <>
+        <section>
+          {
+            this.state.selectedVideo ? (<MainVideo video={this.state.selectedVideo}/>) : (<p>Loading...</p>)
+          }
+        </section>
+        <section className='page-components'>
+          <section className='page-components__main-video'>
             {
-              this.state.selectedVideo ? (<MainVideo video={this.state.selectedVideo}/>) : (<p>Loading...</p>)
+              this.state.selectedVideo ? (<MainVideoDetails video={this.state.selectedVideo} date={bestDate}/>) : ('')
+            }
+            {
+              this.state.selectedVideo ? (<Form video={this.state.selectedVideo}/>) : ('')
+            }
+            {
+              this.state.selectedVideo ? (<Comments comments={this.state.selectedVideo.comments}/>) : ('')
             }
           </section>
-          <section className='page-components'>
-            <section className='page-components__main-video'>
-              {
-                this.state.selectedVideo ? (<MainVideoDetails video={this.state.selectedVideo} date={bestDate}/>) : ('')
-              }
-              {
-                this.state.selectedVideo ? (<Form video={this.state.selectedVideo}/>) : ('')
-              }
-              {
-                this.state.selectedVideo ? (<Comments comments={this.state.selectedVideo.comments}/>) : ('')
-              }
-            </section>
-            <section className='page-components__next-videos'>
-              <NextVideoHeading/>
-              <NextVideo videos={filteredVideos}/>
-            </section>
+          <section className='page-components__next-videos'>
+            <NextVideoHeading/>
+            <NextVideo videos={filteredVideos}/>
           </section>
-        </>
-      )
-    }
+        </section>
+      </>
+    )
+  }
 }
 
 export default HomePage;
