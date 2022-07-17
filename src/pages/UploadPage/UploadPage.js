cde8b54a9b78cd9813d68608bfd99a54de6f627d
Marco Ye <marco.ye99@gmail.com>
2022-07-16 23:20:07 -0400

    turned upload page into function

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -2,18 +2,15 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import UploadForm from '../../components/UploadForm/UploadForm'
 
-class UploadPage extends React.Component {
-    state = {  } 
-    render() { 
-        return (
-            <>
-                <header className='header'>
-                    <Header/>
-                </header>
-                <UploadForm/>
-            </>
-        );
-    }
+const UploadPage = () => {
+    return (
+        <>
+            <header className='header'>
+                <Header/>
+            </header>
+            <UploadForm/>
+        </>
+    );
 }
  
 export default UploadPage;
