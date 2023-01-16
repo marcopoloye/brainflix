25c2d7b3a57b92f396c91c3e3add3855c07b9a39
Marco Ye <marco.ye99@gmail.com>
2023-01-16 04:19:16 -0500

    edited api endpoint

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -16,7 +16,7 @@ const UploadForm = (props) => {
             alert('Please upload a video')
         } else {
             alert('Video Uploaded!');
-            axios.post(`${API_URL}/videos`, {
+            axios.post(`https://brainflix2022api.netlify.app/home/videos`, {
                 title: vidTitle,
                 description: vidDesc
             })
